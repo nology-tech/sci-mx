9c7b4e7e343fcf4c8bd5a99150c1c33df28e0933
EddieSingh <77494320+EddieSingh@users.noreply.github.com>
2021-06-16 15:34:02 +0100

    Began skeleton homepage

--- src/containers/LandingPage/LandingPage.jsx
+++ src/containers/LandingPage/LandingPage.jsx
@@ -3,8 +3,20 @@ import styles from "./LandingPage.module.scss";
 
 const LandingPage = () => {
   return (
-    <main className={styles.hero}>
-      <p>Landing works</p>
+    <main className={styles.container}>
+      <section className={styles.hero}>
+        <h1 className={styles.hero__title}>Hello Will</h1>
+        <div className={styles.hero__image_container}>
+          <img src="../../assets/images/workout/workout-2-image.jpg" alt="" />
+          <button>START WORKOUT</button>
+        </div>
+        <button>
+          My Activity <span>logo</span>
+        </button>
+        <button>
+          <span>ARROW</span>
+        </button>
+      </section>
     </main>
   );
 };
