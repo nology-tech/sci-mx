9466b30fcda61afc30f570cf117e7bac04167f3c
Shruti Battu <shrutibattu@MacBookPro-16inch.local>
2021-06-16 16:47:06 +0100

    added buttons

--- src/containers/LibraryPage/LibraryPage.jsx
+++ src/containers/LibraryPage/LibraryPage.jsx
@@ -5,6 +5,13 @@ const LibraryPage = () => {
   return (
     <div className={styles.container}>
       <input className={styles.searchBar} type="text" placeholder="SEARCH..." />
+
+      <div className={styles.buttoncontainer}>
+        <button className={styles.button}>LEGS</button>
+        <button className={styles.button}>ARMS</button>
+        <button className={styles.button}>CHEST</button>
+        <button className={styles.button}>CARDIO</button>
+      </div>
     </div>
   );
 };
