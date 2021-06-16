0808c29a8a738e31f47d3a99d7502c45df7ae832
carlito-jdp <carlito.jdp@gmail.com>
2021-06-16 16:01:21 +0100

    updated styles for landing page

--- src/containers/LandingPage/LandingPage.jsx
+++ src/containers/LandingPage/LandingPage.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./LandingPage.module.scss";
 
 const LandingPage = () => {
   return (
     <main className={styles.container}>
       <section className={styles.hero}>
-        <h1 className={styles.hero__title}>Hello Will</h1>
+        <h1 className={styles.hero__heading}>Hello Will</h1>
+        <h4 className={styles.hero__subheading}>Welcome back!</h4>
         <div className={styles.hero__image_container}>
           <img src="../../assets/images/workout/workout-2-image.jpg" alt="" />
-          <button>START WORKOUT</button>
+          <Link to="/workout">START WORKOUT</Link>
         </div>
         <button>
           My Activity <span>logo</span>
