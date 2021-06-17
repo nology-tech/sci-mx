790822b8fb2a73c22dd5a6579ad92e7bf24ec533
EddieSingh <77494320+EddieSingh@users.noreply.github.com>
2021-06-17 16:49:36 +0100

    styling second section of homepage

--- src/components/HeroProductSection/HeroProductSection.jsx
+++ src/components/HeroProductSection/HeroProductSection.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 import styles from "./HeroProductSection.module.scss";
+import { Link } from "react-router-dom";
+import { Link as LinkScroll } from "react-scroll";
+import { BiLeftArrow, BiRightArrow } from "react-icons/bi";
+import { FaArrowAltCircleDown } from "react-icons/fa";
+import productImage from "../../assets/images/workout/workout-2-image.jpg";
 
 const HeroProductSection = () => {
   return (
     <section id="product" className={styles.products}>
       <h1>Fuel your workout</h1>
-      <div className="carousel">
-        <button>
-          <img />
-        </button>
-        <img />
-        <button>
-          <img />
-        </button>
+      <div className={styles.carousel}>
+        <BiLeftArrow className={styles.carousel__btn}>
+          <button></button>
+        </BiLeftArrow>
+        <img className={styles.carousel__img} src={productImage} alt="" />
+        <BiRightArrow className={styles.carousel__btn}>
+          <button></button>
+        </BiRightArrow>
       </div>
-      <button>
-        <img />
-      </button>
+      <FaArrowAltCircleDown>
+        <LinkScroll to="#"></LinkScroll>
+      </FaArrowAltCircleDown>
     </section>
   );
 };
