66b9489aa4d644e42d5ead949b434101c590a0c5
carlito-jdp <carlito.jdp@gmail.com>
2021-06-18 17:20:30 +0100

    updated landing page with socials component

--- src/components/HeroSocials/HeroSocials.jsx
+++ src/components/HeroSocials/HeroSocials.jsx
@@ -1,12 +1,36 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaTwitter, FaFacebook, FaYoutube } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
 import styles from "./HeroSocials.module.scss";
 
 const HeroSocials = () => {
   return (
-    <section>
-      <h1>Share your progress</h1>
-      <ul>
-        <li></li>
+    <section className={styles.socials}>
+      <h1 className={styles.socials__heading}>Share your progress</h1>
+      <ul className={styles.socials__list}>
+        <li className={styles.socials__item}>
+          <AiFillInstagram className={styles.socials__icon}>
+            <Link to="/shop" className={styles.socials__link} />
+          </AiFillInstagram>
+        </li>
+        <li className={styles.socials__item}>
+          <FaTwitter className={styles.socials__icon}>
+            <Link to="/shop" className={styles.socials__link} />
+          </FaTwitter>
+        </li>
+        <li className={styles.socials__item}>
+          <FaFacebook className={styles.socials__icon}>
+            <Link to="/shop" className={styles.socials__link} />
+          </FaFacebook>
+        </li>
+        <li className={styles.socials__item}>
+          <FaYoutube
+            className={`${styles.socials__icon} ${styles.socials__icon_youtube}`}
+          >
+            <Link to="/shop" className={styles.socials__link} />
+          </FaYoutube>
+        </li>
       </ul>
     </section>
   );
--- src/containers/LandingPage/LandingPage.jsx
+++ src/containers/LandingPage/LandingPage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import HeroSection from "../../components/HeroSection/HeroSection";
 import HeroProductSection from "../../components/HeroProductSection/HeroProductSection";
 import styles from "./LandingPage.module.scss";
+import HeroSocials from "../../components/HeroSocials/HeroSocials";
 
 const LandingPage = () => {
   return (
     <main className={styles.container}>
       <HeroSection />
       <HeroProductSection />
+      <HeroSocials />
     </main>
   );
 };
