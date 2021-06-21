86052efa17e05de49b69c2a359e58d2a4de5c306
Will Irving <willirving@Wills-Mac-mini.local>
2021-06-21 16:29:09 +0100

    started on layout and styling

--- src/containers/WorkoutPage/WorkoutPage.jsx
+++ src/containers/WorkoutPage/WorkoutPage.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import styles from "./WorkoutPage.module.scss";
+import workoutimage from "../../assets/images/workout/workoutimage.png";
 
 const WorkoutPage = () => {
   return (
     <div className={styles.container}>
-      <p>Workout Page</p>
+      <div className={styles.workoutContainer}>
+        <div className={styles.workoutContainer__title}>NO REST FOR THE WICKED</div>
+        <div className={styles.workoutContainer__rounds}>3 ROUNDS FOR TIME</div>
+        <div className={styles.workoutContainer__exercises}>
+          <ul>
+            <li>15 BURPEES</li>
+            <li>30 AIR SQUATS</li>
+            <li>15 BURPEES</li>
+            <li>30 LUNGES</li>
+            <li>15 BURPEES</li>
+            <li>30 SITUPS</li>
+          </ul>
+        </div>
+        <div className={styles.workoutContainer__image}>
+          <img src={workoutimage} alt="workout"></img>
+        </div>
+      </div>
     </div>
   );
 };
