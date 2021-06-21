31e9fbabca1be3828fe41170e4c4dda7b3829e99
Will Irving <willirving@Wills-Mac-mini.local>
2021-06-21 16:51:51 +0100

    added styling structure and html content

--- src/containers/WorkoutPage/WorkoutPage.jsx
+++ src/containers/WorkoutPage/WorkoutPage.jsx
@@ -8,7 +8,8 @@ const WorkoutPage = () => {
       <div className={styles.workoutContainer}>
         <div className={styles.workoutContainer__title}>NO REST FOR THE WICKED</div>
         <div className={styles.workoutContainer__rounds}>3 ROUNDS FOR TIME</div>
-        <div className={styles.workoutContainer__exercises}>
+        <div className={styles.workoutContainer__body}>
+        <div className={styles.workoutContainer__body__exercises}>
           <ul>
             <li>15 BURPEES</li>
             <li>30 AIR SQUATS</li>
@@ -21,6 +22,8 @@ const WorkoutPage = () => {
         <div className={styles.workoutContainer__image}>
           <img src={workoutimage} alt="workout"></img>
         </div>
+      </div>
+      <div className={styles.workoutContainer__rests}>NO REST BETWEEN ROUNDS</div>
       </div>
     </div>
   );
