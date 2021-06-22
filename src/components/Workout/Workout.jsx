5cf96fca5c04d1c8d80681ad01b85286f2c9ce60
Will Irving <willirving@Wills-Mac-mini.local>
2021-06-22 16:17:58 +0100

    added dynamic data parsing from container to component

--- src/components/Workout/Workout.jsx
+++ src/components/Workout/Workout.jsx
@@ -1,63 +1,40 @@
 import React from "react";
 import styles from "./Workout.module.scss";
-import workoutimage from "../../assets/images/workout/workoutimage.png";
 
-const Workout = () => {
-  const test = {
-    title: "NO REST FOR THE WICKED",
-    rounds: "3 ROUNDS FOR TIME",
-    exercises: [
-      "15 BURPEES",
-      "30 AIR SQUATS",
-      "15 BURPEES",
-      "30 LUNGES",
-      "15 BURPEES",
-      "30 SITUPS",
-    ],
-  };
+const Workout = (props) => {
+
+  const { workout } = props;
+
   const getExerciseJSX = (exercise) => {
     const split = exercise.split(" ");
-    const text = [...split].shift();
+    const letter = split[0];
+    split.shift();
+    // const workoutString = text.join(" ");
+
     return (
       <li>
-        split[0] <span>text.join(" ")</span>
+        {letter} <span>{split.join(" ")}</span>
       </li>
     );
   };
+
   return (
     <>
       <div className={styles.workoutContainer}>
-        <div className={styles.workoutContainer__title}>{test.title}</div>
-        <div className={styles.workoutContainer__rounds}>{test.rounds}</div>
+        <div className={styles.workoutContainer__title}>{workout.title}</div>
+        <div className={styles.workoutContainer__rounds}>{workout.rounds}</div>
         <div className={styles.workoutContainer__body}>
           <div className={styles.workoutContainer__body__exercises}>
             <ul>
-              <li>
-                15 <span>BURPEES</span>
-              </li>
-              <li>
-                30 <span>AIR SQUATS</span>
-              </li>
-              <li>
-                15 <span>BURPEES</span>
-              </li>
-              <li>
-                30 <span>LUNGES</span>
-              </li>
-              <li>
-                15 <span>BURPEES</span>
-              </li>
-              <li>
-                30 <span>SITUPS</span>
-              </li>
+              {workout.exercises.map(getExerciseJSX)}
             </ul>
           </div>
           <div className={styles.workoutContainer__body__image}>
-            <img src={workoutimage} alt="workout"></img>
+            <img src={workout.img} alt="workout"></img>
           </div>
         </div>
         <div className={styles.workoutContainer__rests}>
-          NO REST BETWEEN ROUNDS
+        {workout.rests}  
         </div>
       </div>
     </>
--- src/containers/WorkoutPage/WorkoutPage.jsx
+++ src/containers/WorkoutPage/WorkoutPage.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import styles from "./WorkoutPage.module.scss";
 import Workout from "../../components/Workout/Workout";
+import workoutimage from "../../assets/images/workout/workoutimage.png";
 
 const WorkoutPage = () => {
+
+  const test = {
+    title: "NO REST FOR THE WICKED",
+    rounds: "3 ROUNDS FOR TIME",
+    exercises: [
+      "15 BURPEES",
+      "30 AIR SQUATS",
+      "15 BURPEES",
+      "30 LUNGES",
+      "15 BURPEES",
+      "30 SITUPS",
+    ],
+    rests: "NO REST BETWEEN ROUNDS",
+    img: workoutimage,
+  };
+
+
   return (
     <div className={styles.container}>
-      <Workout/>
+      <Workout workout={test}/>
     </div>
   );
 };
