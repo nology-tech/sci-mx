fb3c4f6c18ba36d13f04e1acc9671c6f24878b0e
Akexander <alexanderfazan@gmail.com>
2021-06-23 16:14:07 +0100

    change to text border and opacity

--- src/components/Workout/Workout.jsx
+++ src/components/Workout/Workout.jsx
@@ -4,7 +4,6 @@ import fingersBlue from "../../assets/images/Fingers/fingers-blue.png";
 import fingersRed from "../../assets/images/Fingers/fingers-red.png";
 
 const Workout = (props) => {
-
   const { workout } = props;
 
   const getExerciseJSX = (exercise) => {
@@ -23,25 +22,24 @@ const Workout = (props) => {
   return (
     <>
       <div className={styles.workoutContainer}>
-      <img src={fingersRed} alt="fingers2" className={styles.fingers2}></img>
+        <img src={fingersRed} alt="fingers2" className={styles.fingers2}></img>
         <div className={styles.workoutContainer__title}>{workout.title}</div>
         <div className={styles.workoutContainer__rounds}>{workout.rounds}</div>
         <div className={styles.workoutContainer__body}>
           <div className={styles.workoutContainer__body__exercises}>
-            <ul>
-              {workout.exercises.map(getExerciseJSX)}
-            </ul>
+            <ul>{workout.exercises.map(getExerciseJSX)}</ul>
           </div>
         </div>
         <div className={styles.workoutContainer__body__image}>
-            <img src={workout.img} alt="workout" className={styles.workoutpic}></img>
-            <img src={fingersRed} alt="fingers" className={styles.fingers}></img>
-          </div>
-        <div className={styles.workoutContainer__rests}>
-        {workout.rests}  
+          <img
+            src={workout.img}
+            alt="workout"
+            className={styles.workoutpic}
+          ></img>
+          <img src={fingersRed} alt="fingers" className={styles.fingers}></img>
         </div>
+        <div className={styles.workoutContainer__rests}>{workout.rests}</div>
       </div>
-    
     </>
   );
 };
