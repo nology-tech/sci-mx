52d70444947d71888425a5c0241af11eaa09b639
Samuel Harrison <samsmacbook@Samuels-MacBook.local>
2021-06-28 16:59:22 +0100

    added product data file

--- src/components/HeroProductSection/HeroProductSection.jsx
+++ src/components/HeroProductSection/HeroProductSection.jsx
@@ -4,12 +4,32 @@ import styles from "./HeroProductSection.module.scss";
 import { Link as LinkScroll } from "react-scroll";
 import { BiLeftArrow, BiRightArrow } from "react-icons/bi";
 import { FaArrowAltCircleDown } from "react-icons/fa";
-import productImage from "../../assets/images/workout/workout-2-image.jpg";
+import Carousel from "react-multi-carousel";
+import productData from "./data";
+// import productImage from "../../assets/images/workout/workout-2-image.jpg";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 1200, min: 769 },
+    items: 6,
+    slidesToSlide: 3, // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 768, min: 574 },
+    items: 5,
+    slidesToSlide: 2, // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 573, min: 0 },
+    items: 3,
+    slidesToSlide: 1, // optional, default to 1.
+  },
+};
 
 const HeroProductSection = () => {
   return (
     <section id="product" className={styles.products}>
-      <h1 className={styles.products__title}>Fuel your workout</h1>
+      {/* <h1 className={styles.products__title}>Fuel your workout</h1>
       <div className={styles.carousel}>
         <BiLeftArrow
           className={`${styles.carousel__btn} ${styles.carousel__btn_left}`}
@@ -26,7 +46,38 @@ const HeroProductSection = () => {
           <p className={styles.carousel__title}>Daily Mood</p>
           <p className={styles.carousel__price}>£24.99 RRP</p>
         </div>
-      </div>
+      </div> */}
+      <Carousel
+        swipeable={true}
+        draggable={true}
+        // showDots={true}
+        responsive={responsive}
+        infinite={true}
+        className="styles.Carousel__carouselContainer"
+        autoPlaySpeed={3000}
+        keyBoardControl={true}
+        customTransition="all .5"
+        transitionDuration={500}
+        // dotListClass="custom-dot-list-style"
+        // removeArrowOnDeviceType={["tablet", "mobile", "desktop"]}
+        // itemClass="carousel-item-padding-40-px"
+      >
+        {workoutData.map((workout) => (
+          <div className={styles.Carousel__container} key={workout.id}>
+            <div className={styles.Carousel__overlay}></div>
+            <img
+              className={styles.Carousel__image}
+              src={workout.img_path}
+              alt={workout.id}
+            />
+            <div className={styles.Carousel__buttons_container}>
+              <button className={styles.Carousel__button}>
+                {workout.name}
+              </button>
+            </div>
+          </div>
+        ))}
+      </Carousel>
       <LinkScroll className={styles.hero__arrow_btn} to="#">
         <FaArrowAltCircleDown
           className={styles.carousel__arrow_btn}
---
+++ src/components/HeroProductSection/data.js
@@ -0,0 +1,51 @@
+///1 id
+// 2 image
+// 3 name
+// 4 price
+// 5 link
+
+import Product1 from "src/assets/images/Capsules/DAILY_MOOD_BOOSTER_90_CAPS-3500x3500.png";
+import Product2 from "src/assets/images/Capsules/IMMUNE_BOOSTER_60_CAPS-3500x3500.png";
+import Product3 from "src/assets/images/Capsules/SHRED-X_DIET_SUPPORT_90_CAPS-3500x3500.png";
+import Product4 from "src/assets/images/Capsules/TESTOSTERONE_SUPPORT_120_CAPS-3500x3500.png";
+import Product5 from "src/assets/images/Capsules/ULTIMATE_JOINT_SUPPORT_60_CAP-3500x3500.png";
+
+const productData = [
+  {
+    id: "1",
+    img_path: Product1,
+    name: "Daily Mood Booster Capsules",
+    price: 0,
+    link: "https://sci-mx.co.uk/products/omni-mx-mass",
+  },
+  {
+    id: "2",
+    img_path: Product2,
+    name: "Immune Booster Capsules",
+    price: 0,
+    link: "https://sci-mx.co.uk/products/omni-mx-mass",
+  },
+  {
+    id: "3",
+    img_path: Product3,
+    name: "Shred-X Diet Support Capsules",
+    price: 0,
+    link: "https://sci-mx.co.uk/products/omni-mx-mass",
+  },
+  {
+    id: "4",
+    img_path: Product4,
+    name: "Testosterone Support Capsules",
+    price: 0,
+    link: "https://sci-mx.co.uk/products/omni-mx-mass",
+  },
+  {
+    id: "5",
+    img_path: Product5,
+    name: "Ultimate Joint Support Capsules",
+    price: 0,
+    link: "https://sci-mx.co.uk/products/omni-mx-mass",
+  },
+];
+
+export default productData;
