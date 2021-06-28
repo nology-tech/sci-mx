dbd6442f08e487b20a5bb51d22b1818833e77559
Charlie Richardson <charlie.robin.richardson@gmail.com>
2021-06-28 14:29:11 +0100

    slight css tweaks

--- src/components/Workout/Workout.jsx
+++ src/components/Workout/Workout.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Workout.module.scss";
 import fingersBlue from "../../assets/images/Fingers/fingers-blue.png";
 import fingersRed from "../../assets/images/Fingers/fingers-red.png";
@@ -7,54 +7,49 @@ import fingersYellow from "../../assets/images/Fingers/fingers-yellow.png";
 import fingersOrange from "../../assets/images/Fingers/fingers-orange.png";
 import fingersLightBlue from "../../assets/images/Fingers/fingers-light-blue.png";
 
-
-const Workout = (props) => {
-
+const Workout = props => {
   const getRandomInt = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  };
 
   useEffect(() => {
-    const int = getRandomInt(1,6);
+    const int = getRandomInt(1, 6);
 
-    switch(int){
+    switch (int) {
       case 1:
         setFingers(fingersRed);
         break;
-        case 2:
-          setFingers(fingersBlue)
-          break;
-          case 3:
-          setFingers(fingersGreen)
-          break;
-          case 4:
-          setFingers(fingersYellow)
-          break;
-          case 5:
-          setFingers(fingersOrange)
-          break;
-          case 6:
-          setFingers(fingersLightBlue)
-          break;
-          default:
-            setFingers(fingersBlue);
+      case 2:
+        setFingers(fingersBlue);
+        break;
+      case 3:
+        setFingers(fingersGreen);
+        break;
+      case 4:
+        setFingers(fingersYellow);
+        break;
+      case 5:
+        setFingers(fingersOrange);
+        break;
+      case 6:
+        setFingers(fingersLightBlue);
+        break;
+      default:
+        setFingers(fingersBlue);
     }
-  },[])
+  }, []);
 
   const [fingers, setFingers] = useState(fingersBlue);
 
-
   const { workout } = props;
 
-  const getExerciseJSX = (exercise) => {
+  const getExerciseJSX = exercise => {
     const split = exercise.split(" ");
     const letter = split[0];
     split.shift();
     // const workoutString = text.join(" ");
-
-   
 
     return (
       <li>
@@ -70,18 +65,13 @@ const Workout = (props) => {
         <div className={styles.workoutContainer__title}>{workout.title}</div>
         <div className={styles.workoutContainer__rounds}>{workout.rounds}</div>
         <div className={styles.workoutContainer__body}>
-          <div className={styles.workoutContainer__body__exercises}>
-            <ul>{workout.exercises.map(getExerciseJSX)}</ul>
-          </div>
-        </div>
-        <div className={styles.workoutContainer__body__image}>
-          <img
-            src={workout.img}
-            alt="workout"
-            className={styles.workoutpic}
-          ></img>
+          <ul>{workout.exercises.map(getExerciseJSX)}</ul>
+          <img src={workout.img} alt="workout" className={styles.workoutpic}></img>
           <img src={fingers} alt="fingers" className={styles.fingers}></img>
+          {/* <div className={styles.workoutContainer__body__image}>
+          </div> */}
         </div>
+
         <div className={styles.workoutContainer__rests}>{workout.rests}</div>
       </div>
     </>
