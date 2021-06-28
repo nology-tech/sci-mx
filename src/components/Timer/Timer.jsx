c5507577e524e465d1c612b7493d7423d422bddb
Shruti Battu <shrutibattu@MacBookPro-16inch.local>
2021-06-28 16:49:23 +0100

    added timer component

---
+++ src/components/Timer/Timer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Timer.module.scss";
+
+const Timer = () => {
+  return (
+    <>
+      <p>Timer works</p>
+    </>
+  );
+};
+
+export default Timer;
