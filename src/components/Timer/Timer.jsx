0524bbbddf512a0c1f335a49e7a0f2d4a82f6ed9
clint-mctribouy-koy <clintmctribouy@outlook.com>
2021-06-29 14:27:03 +0100

    core functionality for timer

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,11 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Timer.module.scss";
 
 const Timer = () => {
+  const [time, setTime] = useState(0);
+  const [start, setStart] = useState(false);
+  // const [lap, setLap] = useState([]);
+  const timerTracker = [];
+  const lapsContainer = document.querySelector(".lapContainer");
+  useEffect(() => {
+    let interval = null;
+
+    if (start) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [start]);
+
+  let minutes = ("0" + Math.floor((time / 60000) % 60)).slice(-2);
+  let seconds = ("0" + Math.floor((time / 1000) % 60)).slice(-2);
+
   return (
-    <>
-      <p>Timer works</p>
-    </>
+    <section
+      className={styles.timer}
+      onClick={() => {
+        setStart(true);
+      }}
+      onDoubleClick={() => {
+        setStart(false);
+
+        timerTracker.push({ mins: minutes, secs: seconds });
+        console.log(timerTracker);
+      }}
+    >
+      <h1>
+        <span>{minutes}:</span>
+        <span>{seconds}</span>
+      </h1>
+      <button
+        onClick={() => {
+          setTime(0);
+          setStart(true);
+        }}
+      >
+        Reset
+      </button>
+      <div className="lapContainer"></div>
+    </section>
   );
 };
 
