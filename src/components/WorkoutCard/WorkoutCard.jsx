07328d7b6b415fc3aa88b54641100252c028d105
Akexander <alexanderfazan@gmail.com>
2021-06-30 16:05:21 +0100

    Added changes to card styles and workout titles

--- src/components/WorkoutCard/WorkoutCard.jsx
+++ src/components/WorkoutCard/WorkoutCard.jsx
@@ -6,9 +6,9 @@ const WorkoutCard = (props) => {
   const { workoutName } = props;
 
   return (
-    <Link to="/workout/1" className={styles.workout}>
+    <div to="/workout/1" className={styles.workout}>
       <p className={styles.workout__heading}>{workoutName}</p>
-    </Link>
+    </div>
   );
 };
 
--- src/containers/LibraryPage/LibraryPage.jsx
+++ src/containers/LibraryPage/LibraryPage.jsx
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./LibraryPage.module.scss";
 import WorkoutCard from "../../components/WorkoutCard";
 // import workoutimage from "../../assets/images/workout/Group.png";
 
 const LibraryPage = () => {
+  // const [currentCategory, setCurrentCategory] = useState("legs");
+  // switch (currentCategory) {
+  //   case "legs":
+  //     workoutsToRender = "...components";
+  //     break;
+  //   default:
+  //     workoutsToRender = "...components";
+  //     break;
+  // }
+
+  //datafile --> workouts.map(workout => <WorkoutCard workoutName="Jump Squats" /> )
+
   return (
     <div className={styles.container}>
-      {/* <input className={styles.searchBar} type="text" placeholder="SEARCH..." />
+      {/* <input className={styles.searchBar} type="text" placeholder="SEARCH..." />*/}
 
-      <div className={styles.buttoncontainer}>
-        <button className={styles.buttoncontainer__button}>LEGS</button>
-        <button className={styles.buttoncontainer__button}>ARMS</button>
-        <button className={styles.buttoncontainer__button}>CHEST</button>
-        <button className={styles.buttoncontainer__button}>CARDIO</button>
-      </div> */}
+      <div className={styles.categories}>
+        <ul className={styles.categories__lists}>
+          <li className={styles.categories__items}>
+            <button className={`${styles.categories__link}`}>Legs</button>
+          </li>
+          <li className={styles.categories__items}>
+            <button className={styles.categories__link}>Arms</button>
+          </li>
+          <li className={styles.categories__items}>
+            <button className={styles.categories__link}>Chest</button>
+          </li>
+          <li className={styles.categories__items}>
+            <button className={styles.categories__link}>Back</button>
+          </li>
+        </ul>
+      </div>
 
       <div className={styles.workouts}>
         <WorkoutCard workoutName="Jump Squats" />
-        <WorkoutCard workoutName="Bulgarian Split Squats" />
+        <WorkoutCard workoutName="Split Squats" />
         <WorkoutCard workoutName="Pistol Squats" />
         <WorkoutCard workoutName="Single Leg Squats" />
         <WorkoutCard workoutName="Up-down Squats" />
+        <WorkoutCard workoutName="W Superman" />
       </div>
     </div>
   );
