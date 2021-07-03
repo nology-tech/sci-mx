fc8a408f8ed8c1e895120b79ac23ad60ecf54ef9
Charlie Richardson <charlie.robin.richardson@gmail.com>
2021-07-03 22:38:22 +0100

    Got the timer animation working

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -5,25 +5,7 @@ const Timer = () => {
   const [time, setTime] = useState(0);
   const [start, setStart] = useState(false);
   const [rounds, setRounds] = useState([]);
-  const TIME_LIMIT = 60;
-  let timePassed = 0;
-  let timeLeft = TIME_LIMIT;
-  const FULL_DASH_ARRAY = 283;
 
-  const calculateTimeFraction = () => {
-    return timeLeft / TIME_LIMIT;
-  };
-
-  const setCircleDashArray = () => {
-    const circleDasharray = `${(
-      calculateTimeFraction() * FULL_DASH_ARRAY
-    ).toFixed(0)} 283`;
-    document
-      .getElementById("base-timer-path-remaining")
-      .setAttribute("stroke-dasharray", circleDasharray);
-  };
-
-  // const lapsContainer = document.querySelector(".lapContainer");
   useEffect(() => {
     let interval = null;
 
@@ -45,48 +27,32 @@ const Timer = () => {
       <div className={styles.clockcontainer}>
         <div className={styles.time}>
           <div className={styles.basetimer}>
-            <svg
-              className={styles.basetimer__svg}
-              viewBox="0 0 100 100"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              {/* <svg viewBox="0 0 10 10" x="200" width="100"> */}
-              <g className={styles.basetimercircle}>
-                <circle
-                  className={styles.basetimerpathelapsed}
-                  cx="50"
-                  cy="50"
-                  r="45"
-                />
-                <path
-                  id="base-timer-path-remaining"
-                  stroke-dasharray="283"
-                  className={styles.basetimer__pathremaining}
-                  d="
-          M 50, 50
-          m -45, 0
-          a 45,45 0 1,0 90,0
-          a 45,45 0 1,0 -90,0
-        "
-                >
-                  <animateMotion
-                    // path="M0,0 l300,0"
-                    // begin="0s"
-                    // dur="5s"
-                    // repeatCount="1"
-                    dur="6s"
-                    repeatCount="indefinite"
-                    rotate="auto-reverse"
-                  />
-                </path>
-              </g>
-            </svg>
+          <svg height="110" width="110">
+          <circle 
+            stroke="#ffffff"
+            strokeWidth="10"
+            r="47"
+            cx="55"
+            cy="55"
+            />
+          <circle 
+            stroke="#00263e"
+            strokeDashoffset={-((seconds / 60) * 47 * 2 * Math.PI) + 47 * 2 * Math.PI}
+            strokeDasharray={47 * 2 * Math.PI}
+            strokeWidth="5"
+            fill="#f05224"
+            r="47"
+            cx="55"
+            cy="55"
+          />
+        </svg>
+           
             <span className={styles.basetimerlabel}>
               {minutes}:{seconds}
             </span>
-            {/* </svg> */}
+           
           </div>
-          {/* {minutes}:{seconds} */}
+         
         </div>
       </div>
       <div className={styles.buttoncontainer}>
@@ -113,8 +79,7 @@ const Timer = () => {
               { mins: minutes, secs: seconds, round: rounds.length + 1 },
             ];
             setRounds(newState);
-            console.log(rounds);
-            setCircleDashArray();
+            setTime(0);
           }}
         >
           STOP
