c33a7a70f393ad0cf95013ce20f4d3dad8922cb8
Charlie Richardson <charlie.robin.richardson@gmail.com>
2021-07-05 14:24:49 +0100

    updated timer

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -11,7 +11,7 @@ const Timer = () => {
 
     if (start) {
       interval = setInterval(() => {
-        setTime((prevTime) => prevTime + 10);
+        setTime(prevTime => prevTime + 10);
       }, 10);
     } else {
       clearInterval(interval);
@@ -22,68 +22,55 @@ const Timer = () => {
   let minutes = ("0" + Math.floor((time / 60000) % 60)).slice(-2);
   let seconds = ("0" + Math.floor((time / 1000) % 60)).slice(-2);
 
+  const handleTimerStart = () => {
+    if (!start) {
+      setStart(true);
+    }
+  };
+
+  const handleTimerStop = () => {
+    if (start) {
+      setStart(false);
+      const newState = [...rounds, { mins: minutes, secs: seconds, round: rounds.length + 1 }];
+      setRounds(newState);
+      setTime(0);
+    }
+  };
+
+  const handleTimerReset = () => {
+    setTime(0);
+    setStart(true);
+  };
+
   return (
     <section className={styles.timer}>
       <div className={styles.clockcontainer}>
         <div className={styles.time}>
           <div className={styles.basetimer}>
-          <svg height="110" width="110">
-          <circle 
-            stroke="#ffffff"
-            strokeWidth="10"
-            r="47"
-            cx="55"
-            cy="55"
-            />
-          <circle 
-            stroke="#00263e"
-            strokeDashoffset={-((seconds / 60) * 47 * 2 * Math.PI) + 47 * 2 * Math.PI}
-            strokeDasharray={47 * 2 * Math.PI}
-            strokeWidth="5"
-            fill="#f05224"
-            r="47"
-            cx="55"
-            cy="55"
-          />
-        </svg>
-           
+            <svg class="styles.clockface" height="110" width="110">
+              <circle stroke="#ffffff" strokeWidth="10" r="50" cx="55" cy="55" />
+              <circle
+                stroke="#00263e"
+                strokeDashoffset={-((seconds / 60) * 50 * 2 * Math.PI) + 50 * 2 * Math.PI}
+                strokeDasharray={50 * 2 * Math.PI}
+                strokeWidth="5"
+                fill="#f05224"
+                r="50"
+                cx="55"
+                cy="55"
+              />
+            </svg>
+
             <span className={styles.basetimerlabel}>
               {minutes}:{seconds}
             </span>
-           
           </div>
-         
         </div>
       </div>
       <div className={styles.buttoncontainer}>
-        <button
-          onClick={() => {
-            setTime(0);
-            setStart(true);
-          }}
-        >
-          RESET
-        </button>
-        <button
-          onClick={() => {
-            setStart(true);
-          }}
-        >
-          START
-        </button>
-        <button
-          onClick={() => {
-            setStart(false);
-            const newState = [
-              ...rounds,
-              { mins: minutes, secs: seconds, round: rounds.length + 1 },
-            ];
-            setRounds(newState);
-            setTime(0);
-          }}
-        >
-          STOP
-        </button>
+        <button onClick={handleTimerReset}>RESET</button>
+        <button onClick={handleTimerStart}>START</button>
+        <button onClick={handleTimerStop}>STOP</button>
       </div>
       <div className={styles.roundcontainer}>
         {rounds.map((round, index) => (
