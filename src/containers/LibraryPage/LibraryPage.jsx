c87c7bdc7326f6c5f02f075af9664dca1ac455b8
carlito-jdp <carlito.jdp@gmail.com>
2021-07-05 18:09:35 +0100

    updated logic for search bar and updated styling

--- src/containers/LibraryPage/LibraryPage.jsx
+++ src/containers/LibraryPage/LibraryPage.jsx
@@ -6,54 +6,83 @@ import { workoutsCategoryData } from "../../Data/workoutLibraryData";
 
 const LibraryPage = () => {
   const [currentCategory, setCurrentCategory] = useState("legs");
-  const [searchWorkout, setSearchWorkout] = useState("");
+  const [workoutsBySearch, setWorkoutsBySearch] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
-  const handleCategory = e => {
-    console.log(e.target.textContent);
+  const handleCategory = (e) => {
     setCurrentCategory(e.target.textContent);
   };
 
+  const handleSearchQuery = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   const renderByCategories = () => {
-    return workoutsCategoryData[currentCategory].map((workout, i) => <WorkoutCard key={i} workout={workout} />);
+    return workoutsCategoryData[currentCategory].map((workout) => (
+      <WorkoutCard key={workout.id} workout={workout} />
+    ));
   };
 
-  const renderCategoriesBySearch = workouts => {
-    return workouts.map((workout, i) => <WorkoutCard key={i} workout={workout} />);
-  };
-
-  const handleSearchQuery = e => {
-    setSearchWorkout(e.target.value);
-
-    let array = []
-    for (const key in workoutsCategoryData) {
-      const filteredWorkouts = workoutsCategoryData[key].filter(workout => {
-        const bool = workout["exercises"].some(exercise => {
-          return exercise.includes(e.target.value);
-        });
-        return bool;
-      });
-
-      if(filteredWorkouts.length > 0){
-        array = [...array, ...filteredWorkouts];
-      }
+  const renderWorkoutsBySearchTerm = () => {
+    if (workoutsBySearch.length > 0) {
+      return workoutsBySearch.map((workout) => (
+        <WorkoutCard key={workout.id} workout={workout} />
+      ));
+    } else {
+      return (
+        <p className={styles.workouts__not_found}>Cannot find that work...</p>
+      );
     }
-    return renderCategoriesBySearch(array);
   };
+
+  const getWorkoutsBySearchTerm = () => {
+    let workouts = [];
+    for (const key in workoutsCategoryData) {
+      workoutsCategoryData[key].forEach((workout) => {
+        const exercises = workout["exercises"].join(" ").toUpperCase();
+        const isTermInWorkout = exercises.includes(searchTerm.toUpperCase());
+
+        if (isTermInWorkout) {
+          workouts.push(workout);
+        }
+      });
+    }
+    console.log(workouts);
+    setWorkoutsBySearch([...workouts]);
+  };
+
+  useEffect(() => {
+    getWorkoutsBySearchTerm();
+  }, [searchTerm]);
 
   return (
     <div className={styles.container}>
-      <input type="text" placeholder="search workout" onChange={handleSearchQuery} />
+      <div className={styles.search_container}>
+        <input
+          type="text"
+          placeholder="search for a workout..."
+          value={searchTerm}
+          onChange={handleSearchQuery}
+          className={styles.search_container__search_input}
+        />
+      </div>
 
       <div className={styles.categories}>
         <ul className={styles.categories__lists}>
-          {Object.keys(workoutsCategoryData).map(category => (
-            <WorkoutCategories key={category} handleCategory={handleCategory} category={category} />
+          {Object.keys(workoutsCategoryData).map((category) => (
+            <WorkoutCategories
+              key={category}
+              handleCategory={handleCategory}
+              category={category}
+            />
           ))}
         </ul>
       </div>
 
       <div className={styles.workouts}>
-        {searchWorkout.trim().length > 0 ? handleSearchQuery() : renderByCategories()}
+        {searchTerm.trim().length > 0
+          ? renderWorkoutsBySearchTerm()
+          : renderByCategories()}
       </div>
     </div>
   );
