02f52b7169a2afcd223f8a8312cd96b1cc3f5df8
Charlie Richardson <charlie.robin.richardson@gmail.com>
2021-07-08 20:41:10 +0100

    Added reducing firestore data to match graph data

--- src/context/WorkoutProvider/WorkoutProvider.jsx
+++ src/context/WorkoutProvider/WorkoutProvider.jsx
@@ -1,17 +1,11 @@
-import React, {
-  createContext,
-  useCallback,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
+import React, { createContext, useCallback, useContext, useEffect, useState } from "react";
 import { firestore } from "../../firebase";
 import { UserContext } from "../UserProvider/UserProvider";
 import emptyWorkout from "../../Data/emptyWorkout/emptyWorkout";
 
 export const WorkoutContext = createContext({});
 
-const WorkoutProvider = (props) => {
+const WorkoutProvider = props => {
   const { user } = useContext(UserContext);
   const [workoutData, setWorkoutData] = useState({});
   const workCollection = "WorkoutData";
@@ -21,7 +15,7 @@ const WorkoutProvider = (props) => {
       .collection(workCollection)
       .doc("WsybJoJaePYD3ld8sFaxEkZR3Rq1")
       .get()
-      .then((querySnapshot) => {
+      .then(querySnapshot => {
         if (querySnapshot.exists) {
           const data = querySnapshot.data();
           setWorkoutData(data);
@@ -37,13 +31,54 @@ const WorkoutProvider = (props) => {
     }
   }, [user, getWorkoutData]);
 
-  const contextData = { workoutData };
+  const isDateInCurrentWeek = date => {
+    const todayDateObject = new Date();
+    const getDate = new Date().getDate();
+    const getDay = new Date().getDay();
 
-  return (
-    <WorkoutContext.Provider value={contextData}>
-      {props.children}
-    </WorkoutContext.Provider>
+    const firstDayOfWeek = new Date(todayDateObject.setDate(getDate - getDay));
+    const lastDayOfWeek = new Date(firstDayOfWeek);
+
+    lastDayOfWeek.setDate(lastDayOfWeek.getDate() + 6);
+
+    return date >= firstDayOfWeek && date <= lastDayOfWeek;
+  };
+
+  const getDayOfWeek = index => ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"][index];
+
+  const weeksWorkouts = workoutData.workoutArray?.reduce(
+    (weekWorkouts, workOut) => {
+      const { dateCompleted, workoutsCompleted } = workOut;
+      const workoutDate = dateCompleted.toDate();
+
+      if (!isDateInCurrentWeek(workoutDate)) {
+        return weekWorkouts;
+      }
+
+      const day = getDayOfWeek(workoutDate.getDay());
+
+      weekWorkouts[day] = workoutsCompleted;
+
+      return weekWorkouts;
+    },
+    { sunday: 0, monday: 0, tuesday: 0, wednesday: 0, thursday: 0, friday: 0, saturday: 0 }
   );
+
+  const weeksGraphData = [
+    weeksWorkouts?.sunday,
+    weeksWorkouts?.monday,
+    weeksWorkouts?.tuesday,
+    weeksWorkouts?.wednesday,
+    weeksWorkouts?.thursday,
+    weeksWorkouts?.friday,
+    weeksWorkouts?.saturday,
+  ];
+
+  const weeksWorkoutsTotal = weeksGraphData.reduce((total, current) => total + current, 0);
+
+  const contextData = { workoutData, weeksGraphData, weeksWorkoutsTotal };
+
+  return <WorkoutContext.Provider value={contextData}>{props.children}</WorkoutContext.Provider>;
 };
 
 export default WorkoutProvider;
